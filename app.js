545ae21bf01d649378a9803700c1f94e44cf17d8
augustopgsm2023 <132454102+augustopgsm2023@users.noreply.github.com>
2024-10-10 11:39:53 +0000

    alteraçoes

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ criarCartao(categoria, pergunta, resposta) {
 }
 function criarCartao(categoria, pergunta, resposta) {
 }
-function criaCartao(categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta);
-    }
+    function criaCartao(categoria, pergunta, resposta) {
+
+        let container =  document.getElementById(‘container’);
+        let cartao = document.createElement(‘article’);
+        cartao.className = ‘cartao’;
+        
+        }
+        container.appendChild(cartao)
+        
